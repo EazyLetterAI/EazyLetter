927539c30facdf414fb93b3a103c4fa9ad0dd371
Eliora Kruman <krumaneliora@gmail.com>
2024-03-31 22:17:27 -0400

    main page set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,31 +21,31 @@ export default async function Home() {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-2 hover:bg-white/20"
+              href="/resume"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Create my Resume →</h3>
+              <h3 className="text-2xl font-bold text-[hsl(280,100%,70%)]">Create my Resume →</h3>
               <div className="text-lg">
                 Generate the perfect professional resume in minutes with the help of our AI. Certified ATS compliant.
               </div>
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-2 hover:bg-white/20"
+              href="/cover"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Write my Cover Letter →</h3>
+              <h3 className="text-2xl font-bold text-[hsl(280,100%,70%)]">Write my Cover Letter →</h3>
               <div className="text-lg">
                 Construct a cover letter to land your next interview with our AI. Certified ATS compliant.
               </div>
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-2 hover:bg-white/20"
+              href="/interview"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Interview Me →</h3>
+              <h3 className="text-2xl font-bold text-[hsl(280,100%,70%)]">Interview Me →</h3>
               <div className="text-lg">
                 Practice your interview questions with our AI's voice-to-text recognition software.
               </div>
@@ -63,15 +63,6 @@ export default async function Home() {
           </a>
         </div>
       </div> */}
-      <div className="cover">
-
-      </div>
-      <div className="interview">
-
-      </div>
-      <div className="pricing">
-
-      </div>
       {/*format: resume -> cover letter -> interview -> pricing*/}
       <div className="footer bg-cyan-800 text-white py-4">
       <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-4">
