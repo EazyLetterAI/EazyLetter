eb928d05460ee54643dda68dff9430e5e0eda050
kdot <kevincui221@gmail.com>
2024-04-01 11:19:10 -0400

    current changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 import { getServerAuthSession } from "~/server/auth";
 import { SocialIcon } from "react-social-icons"
+import Graphic1 from "../../public/homepage1.png";
 
 
 import { HeaderBar } from "~/app/_components/header-bar";
@@ -99,6 +102,39 @@ export default async function Home() {
         </div>
       </div>
     </div>
+      <div className="top-0 flex w-full flex-row justify-between">
+        <div className="w-1/2 ">
+        <p className="text-4xl font-bold">Your dream job starts<br /><span style={{ color: "#2a49e4" }}> with a great resume</span>.</p>
+        <p className="text-gray-600">Create professional resume and cover letters with the help of artificial intelligence and our ATS-compliant templates.</p>
+        <button className="bg-gradient-linear rounded-md py-2 px-6">
+          <p className="text-white text-xl">Create My Resume</p>
+        </button>
+        <p className="text-blue-800">Join over 5000 students that trusted us to land their first job.</p>
+        </div>
+        <div className="w-1/2">
+          <Image src={Graphic1} alt="picture" />
+        </div>
+      </div>
+      {/* Pricing */}
+      <div className="PricingSection text-center">
+        <p className="text-fuchsia-500 text-2xl font-medium">Pricing</p>
+        <p className="text-black text-5xl font-medium">Choose your plan</p>
+        <div className="top-0 flex w-full flex-row justify-between">
+          <div className="w-1/2">
+            <p>YOOYOY</p>
+          </div>
+          <div className="w-1/3">
+            <p>YOOYOY</p>
+          </div>
+          <div className="w-1/3">
+            <p>YOOYOY</p>
+          </div>
+        </div>
+      </div>
+
+
+  
     </main>
   );
 }
+
