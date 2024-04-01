999d90619aca2924efde85503a3d2ce89ac73f08
Eliora Kruman <krumaneliora@gmail.com>
2024-03-31 21:48:03 -0400

    initial front page sketch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { SocialIcon } from "react-social-icons"
 
 
 import { HeaderBar } from "~/app/_components/header-bar";
-import { Footer } from "~/app/_components/footer";
 
 export default async function Home() {
   const session =  await getServerAuthSession();
@@ -14,25 +13,21 @@ export default async function Home() {
       <div className="flex">
         <HeaderBar session = { session }/>
       </div>
-      <div className="footer">
-        <Footer session={ session }/>
-      </div>
       <div className="flex min-h-screen flex-col items-center justify-center text-black"> 
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Your <span className="text-[hsl(280,100%,70%)]">dream job</span> starts here.
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">Create my Resume →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Generate the perfect professional resume in minutes with the help of our AI. Certified ATS compliant.
               </div>
             </Link>
             <Link
@@ -40,15 +35,44 @@ export default async function Home() {
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-bold">Write my Cover Letter →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how to
-                deploy it.
+                Construct a cover letter to land your next interview with our AI. Certified ATS compliant.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Interview Me →</h3>
+              <div className="text-lg">
+                Practice your interview questions with our AI's voice-to-text recognition software.
               </div>
             </Link>
           </div>
         </div>
       </div>
+      {/* <div className="resume">
+        <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-4">
+          <a
+            href="/resume"
+            className="text-center px-4 py-2 rounded-lg bg-cyan-600 hover:bg-cyan-700 transition-colors"
+          >
+            Create my Resume
+          </a>
+        </div>
+      </div> */}
+      <div className="cover">
+
+      </div>
+      <div className="interview">
+
+      </div>
+      <div className="pricing">
+
+      </div>
+      {/*format: resume -> cover letter -> interview -> pricing*/}
       <div className="footer bg-cyan-800 text-white py-4">
       <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-4">
         <div className="grid grid-cols-1 sm:grid-cols-5 gap-6">
